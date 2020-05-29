293400fcef9622d313e4eb96439cd80d9bb60666
Alice Kim <aliceykim0828@gmail.com>
2020-05-29 12:28:47 -0700

    Add eslint

---
+++ client/.eslintrc.js
@@ -0,0 +1,44 @@
+const OFF = 0;
+const ON = 1;
+const ERROR = 2;
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb-base', 'airbnb/rules/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-case-declarations': OFF,
+    'no-irregular-whitespace': OFF,
+    'no-param-reassign': OFF,
+    'no-restricted-globals': OFF,
+    'no-shadow': OFF,
+    'no-undef': OFF,
+    'no-underscore-dangle': OFF,
+    'no-unused-vars': [ON, { args: 'none' }],
+    'no-use-before-define': OFF,
+    'react/prop-types': OFF,
+    'react/jsx-indent': [ON, 4],
+    'react/jsx-indent-props': OFF,
+    'react/jsx-filename-extension': OFF,
+    'react/jsx-max-props-per-line': [ON, { maximum: 5, when: 'multiline' }],
+    'react/jsx-props-no-spreading': OFF,
+    'react/no-array-index-key': OFF,
+    'react/no-unescaped-entities': OFF,
+    indent: OFF,
+    quotes: [ERROR, 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+  },
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 import reducers from './reducers';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider
-			store={createStore(
-				reducers,
-				window.__REDUX_DEVTOOLS_EXTENSION__ &&
-					window.__REDUX_DEVTOOLS_EXTENSION__()
-			)}
-		>
-			<App />
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById('root')
+    <React.StrictMode>
+        <Provider
+            store={createStore(
+                reducers,
+                window.__REDUX_DEVTOOLS_EXTENSION__
+                && window.__REDUX_DEVTOOLS_EXTENSION__(),
+            )}
+        >
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
