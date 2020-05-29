d2270fd1141be9b12a009b5e6bb393952a50dc46
Alice Kim <aliceykim0828@gmail.com>
2020-05-29 15:06:16 -0700

    Add Material UI core/icon/styles

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,43 +1,71 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import src from '../assets/hug.jpg';
 
-const Square = ({ title, subtitle, to }) => (
-    <Link to={to}>
-        <div style={{
-            background: '#0f2c70', width: 380, height: 340, padding: '10px 10px 50px', textAlign: 'center', borderRadius: 7.5, display: 'flex', flexDirection: 'column-reverse', color: '#fbebe3'
-        }}
-        >
-            <div>
-                <img src={src} style={{
-                    borderRadius: '50%',
-                    width: 300,
-                    height: 300,
-                    objectFit: 'cover',
-                }} />
-                <h3>{title}</h3>
-                <p style={{ fontSize: 'small' }}>{subtitle}</p>
-            </div>
-        </div>
-    </Link>
-);
+const useStyles = makeStyles({
+    header: {
+        fontFamily: '\'PT Serif Caption\', serif',
+        textAlign: 'center',
+    },
+    container: {
+        display: 'flex',
+        width: 926,
+        margin: 'auto',
+    },
+    square: {
+        background: '#0f2c70',
+        width: 380,
+        height: 340,
+        padding: '10px 10px 50px',
+        textAlign: 'center',
+        borderRadius: 7.5,
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        color: '#fbebe3',
+    },
+    img: {
+        borderRadius: '50%',
+        width: 300,
+        height: 300,
+        objectFit: 'cover',
+    },
+});
 
-const Home = () => (
-    <>
-        <h1 style={{ fontFamily: '\'PT Serif Caption\', serif', textAlign: 'center' }}>Are you...</h1>
-        <div style={{ display: 'flex', width: 926, margin: 'auto' }}>
-            <Square
-                title="Business Owner"
-                subtitle="We need a nice logo for our website!"
-                to="/businesses"
-            />
-            <Square
-                title="Volunteer"
-                subtitle="Looking to get involved in the community!"
-                to="/volunteers"
-            />
-        </div>
-    </>
-);
+const Square = ({ title, subtitle, to }) => {
+    const classes = useStyles();
+    return (
+        <Link to={to}>
+            <div className={classes.square}>
+                <div>
+                    <img src={src} className={classes.img} />
+                    <h3>{title}</h3>
+                    <p style={{ fontSize: 'small' }}>{subtitle}</p>
+                </div>
+            </div>
+        </Link>
+    );
+};
+
+const Home = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <h1 className={classes.header}>Are you...</h1>
+            <div className={classes.container}>
+                <Square
+                    title="Business Owner"
+                    subtitle="We need a nice logo for our website!"
+                    to="/businesses"
+                />
+                <Square
+                    title="Volunteer"
+                    subtitle="Looking to get involved in the community!"
+                    to="/volunteers"
+                />
+            </div>
+        </>
+    );
+};
 
 export default Home;
