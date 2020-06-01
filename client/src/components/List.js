749421b27d5b456c44131194e412afd1db947453
Hyesun An <hyesun@alumni.ubc.ca>
2020-05-31 20:40:01 -0700

    Create filtering tags (#12)
    #### [Create filtering tags](https://github.com/CPSC436/ThinkSmall/issues/7)
    **What and how** 
    
    - Create clickable `Tags` component 
    - Applied mock `tags` list (not actual filtering functionality) 
    
    **Screenshots** 
    ![tags](https://user-images.githubusercontent.com/39198761/83373142-3d6c4780-a37c-11ea-9903-275be5fb82b6.PNG)

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Tags from './Tags';
 
 const List = ({ businesses, volunteers }) => (
     <>
         {businesses && 'businesses'}
         {volunteers && 'volunteers'}
+        <Tags />
     </>
 );
 
---
+++ client/src/components/Tags.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
+
+const tags = [
+  { label: 'Near me' },
+  { label: 'Urgent', color: 'primary' },
+  { label: 'Call for designers', color: 'primary' },
+  { label: 'Call for developers', color: 'primary' },
+  { label: 'call for teanslators', color: 'secondary' },
+];
+
+export default function Chips() {
+  const classes = useStyles();
+  return (
+      <div className={classes.root}>
+          {tags.map(({ label, color }, i) => (
+              <Chip label={label} clickable color={color} />
+      ))}
+      </div>
+  );
+}
