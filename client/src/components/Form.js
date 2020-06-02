80143a7c9aac0747202db9c105e90487dde1ff67
Alice Kim <aliceykim0828@gmail.com>
2020-06-01 20:53:12 -0700

    Fix indentation AND create Form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
+<<<<<<< HEAD
 import { useTheme, withStyles, makeStyles } from '@material-ui/core/styles';
 import {
     Button,
@@ -144,6 +145,26 @@ const Form = () => {
             {address && <Suggestions {...props} />}
         </div>
     );
+=======
+import { useTheme } from '@material-ui/core/styles';
+import {
+    Dialog,
+    DialogTitle as Title,
+    DialogContent as Content,
+    DialogContentText as ContentText,
+    TextareaAutosize as Textarea,
+    Typography as Text,
+    OutlinedInput as Input,
+} from '@material-ui/core';
+import Tags from './Tags';
+
+const Form = () => {
+    const [open, setOpen] = useState(true);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClose = () => setOpen(false);
+>>>>>>> 69494d1... Fix indentation AND create Form component
 
     return (
         <Dialog
@@ -152,6 +173,7 @@ const Form = () => {
             onClose={handleClose}
             aria-labelledby="request-form"
         >
+<<<<<<< HEAD
             <Title id="request-form" disableTypography>File a new request</Title>
             <Content>
                 <ContentText>
@@ -177,5 +199,26 @@ const Form = () => {
         </Dialog>
     );
 };
+=======
+            <Title id="request-form">File a new request</Title>
+            <Content style={{ marginBottom: 30 }}>
+                <ContentText>
+                    To help us best understand your situation, please provide details and type of help
+                    required of your business.
+                </ContentText>
+                <Text>Name of Business</Text>
+                <Input autoFocus margin="dense" fullWidth />
+                <Text>Location</Text>
+                <Input margin="dense" fullWidth />
+                <Text>Description of Business</Text>
+                <Textarea aria-label="description" rowsMin={5} style={{ width: 'calc(100% - 5px)', fontFamily: 'sans-serif' }} />
+                <Text>Request Details</Text>
+                <Tags />
+                <Textarea aria-label="details" rowsMin={5} style={{ width: 'calc(100% - 5px)', fontFamily: 'sans-serif' }} />
+            </Content>
+        </Dialog>
+    );
+}
+>>>>>>> 69494d1... Fix indentation AND create Form component
 
 export default Form;
