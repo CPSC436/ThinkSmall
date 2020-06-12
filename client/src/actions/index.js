5708fc31249c1ee82763cfb6a7eab70784f52191
Yuree <yj.yuree.jang@gmail.com>
2020-06-11 18:56:33 -0700

    Naive implementation of businesses reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,8 @@
 export const PUSH_MESSAGE = 'PUSH_MESSAGE';
 export const SET_CONVERSATION = 'SET_CONVERSATION';
+export const ADD_BUSINESS = 'ADD_BUSINESS';
+export const DELETE_BUSINESS = 'DELETE_BUSINESS';
+export const EDIT_BUSINESS = 'EDIT_BUSINESS';
 
 export const push = (uid, iid, message) => ({
     type: PUSH_MESSAGE,
@@ -11,4 +14,34 @@ export const push = (uid, iid, message) => ({
 export const setConversation = id => ({
     type: SET_CONVERSATION,
     id,
+});
+
+export const addBusiness = (storeName, avatar, storeOwner, location, description,
+                            needsHelp, tags) => ({
+    type: ADD_BUSINESS,
+    storeName,
+    avatar,
+    storeOwner,
+    location,
+    description,
+    needsHelp,
+    tags,
+});
+
+export const deleteBusiness = id => ({
+    type: DELETE_BUSINESS,
+    id,
+});
+
+export const editBusiness = (id, storeName, avatar, storeOwner, location, description,
+                             needsHelp, tags) => ({
+    type: EDIT_BUSINESS,
+    id,
+    storeName,
+    avatar,
+    storeOwner,
+    location,
+    description,
+    needsHelp,
+    tags,
 });
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,7 +2,11 @@ import { combineReducers } from 'redux';
 import {
     PUSH_MESSAGE,
     SET_CONVERSATION,
+    ADD_BUSINESS,
+    DELETE_BUSINESS,
+    EDIT_BUSINESS,
 } from '../actions';
+import { businesses, volunteers } from '../constant';
 import { now } from './helper';
 
 const defaultUsers = [
@@ -123,5 +127,51 @@ export default combineReducers({
             default:
                 return conversation;
         }
+    },
+    businesses: (businesses = businesses, action) => {
+        switch (action.type) {
+            case ADD_BUSINESS:
+                const {
+                    storeName, avatar, storeOwner, location, description,
+                    needsHelp, tags,
+                } = action;
+                const id = Math.random();
+                return Object.assign([...businesses], {
+                    [id]: id,
+                    [storeName]: storeName,
+                    [avatar]: avatar,
+                    [storeOwner]: storeOwner,
+                    [location]: location,
+                    [description]: description,
+                    [needsHelp]: needsHelp,
+                    [tags]: tags,
+                });
+            case DELETE_BUSINESS:
+                const updatedBusinesses = [...businesses];
+                return delete updatedBusinesses[action.id];
+            case EDIT_BUSINESS:
+                const editedUsers = [...businesses];
+                const properties = [storeName,
+                    avatar,
+                    storeOwner,
+                    location,
+                    description,
+                    needsHelp,
+                    tags];
+                for (let i = 0; i < editedUsers.length; i++) {
+                    if (editedUsers[i].id === action.id) {
+                        for (let j = 0; j < 7; j++) {
+                            editedUsers[i].properties[j] = properties[j];
+                        }
+                        break;
+                    }
+                }
+                return editedUsers;
+            default:
+                return users;
+        }
+    },
+    volunteers: (volunteers = volunteers, action) => {
+
     },
 });
