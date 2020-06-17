8bab7c1be100f70753afca0497fb41d6ae95c840
John <johnskimca@gmail.com>
2020-06-17 13:05:31 -0700

    .env.local finished and switch added but cut off

--- client/src/components/Maps.js
+++ client/src/components/Maps.js
@@ -51,13 +51,15 @@ function Map() {
 
 // must wrap the map with some more code for google maps to work
 const WrappedMap = withScriptjs(withGoogleMap(Map));
+const API_KEY = process.env.REACT_APP_API_KEY;
 // {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
+// ask about witch and why display map cant be componentized
 
 function Maps() {
     const displayMap = () => (
         <div style={{ width: '100vw', height: '100vh' }}>
             <WrappedMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=REACT_APP_API_KEY"
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${API_KEY}`}
                 loadingElement={<div style={{ height: '100%' }} />}
                 containerElement={<div style={{ height: '100%' }} />}
                 mapElement={<div style={{ height: '100%' }} />}
