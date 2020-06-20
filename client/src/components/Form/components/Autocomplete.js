88b7c5c3cae556edb4dfdf774ce7f6c5c20a5ebc
Alice <aliceykim0828@gmail.com>
2020-06-20 01:11:30 -0700

    apply mild styling (#48)
    LGTM!

---
+++ client/src/components/Form/components/Autocomplete.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import PlacesAutocomplete from 'react-places-autocomplete';
+import LoadingIndicator from './LoadingIndicator';
+import { styles } from '../../../constant';
+import classes from '../../../modules/form.module.css';
+
+const Autocomplete = ({ value, onChange, onSelect }) => {
+    const Suggestions = ({ loading, suggestions, getSuggestionItemProps }) => (
+        <div className={['autocomplete-dropdown-container', classes.suggestions]}>
+            {loading
+                ? <LoadingIndicator />
+                : suggestions.map(suggestion => {
+                    const className = suggestion.active
+                        ? 'suggestion-item--active'
+                        : 'suggestion-item';
+                    const style = suggestion.active
+                        ? styles.active
+                        : styles.inactive;
+                    return (
+                        <div
+                            {...getSuggestionItemProps(suggestion, {
+                                className,
+                                style,
+                            })}
+                        >
+                            <span>{suggestion.description}</span>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+    const PlacesInput = ({ getInputProps, ...props }) => (
+        <div>
+            <input
+                {...getInputProps({
+                    placeholder: 'Search Places ...',
+                    className: 'location-search-input',
+                })}
+                className={classes.input}
+            />
+            {value && <Suggestions {...props} />}
+        </div>
+    );
+    return (
+        <PlacesAutocomplete {...{ value, onChange, onSelect }}>
+            {PlacesInput}
+        </PlacesAutocomplete>
+    );
+};
+
+export default Autocomplete;
