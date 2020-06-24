5ddc5ff43953b6cbf7e636dab81c34fe9818836d
Alice Kim <aliceykim0828@gmail.com>
2020-06-24 16:33:39 -0700

    add mongoose, dotenv AND create sample workflow for fetching data from db

---
+++ api/db/index.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const path = require('path')
+
+require('dotenv').config({ path: path.join(__dirname, '.env') })
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@sandbox-vfnah.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose
+    .connect(uri, { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
