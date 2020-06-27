a7f8282b6c7b0fadc50fed60cc9e15897486762b
Alice Kim <aliceykim0828@gmail.com>
2020-06-26 20:11:22 -0700

    set user's current location as default center

--- client/src/components/Form/components/LoadingIndicator.js
+++ client/src/components/Form/components/LoadingIndicator.js
@@ -2,7 +2,12 @@ import React from 'react'
 import loader from '../../../assets/loader.svg'
 
 const LoadingIndicator = () => (
-    <div style={{ padding: 50 }}>
+    <div style={{
+        margin: 'auto',
+        position: 'relative',
+        top: '50%',
+    }}
+    >
         <img id="loading" src={loader} />
         <p style={{ textAlign: 'center', fontSize: 'small' }}>Loading...</p>
     </div>
