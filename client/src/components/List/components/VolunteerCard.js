63af94541dfeeec087befcf892273949ded7fa74
Alice Kim <aliceykim0828@gmail.com>
2020-06-26 21:44:38 -0700

    implement actions/reducers for filtering using tags

--- client/src/components/List/components/VolunteerCard.js
+++ client/src/components/List/components/VolunteerCard.js
@@ -6,8 +6,8 @@ import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider'
-import Tags from './Tags/Tags'
-import classes from '../modules/card.module.css'
+import Tags from '../../Tags/Tags'
+import classes from '../../../modules/card.module.css'
 
 const VolunteerCard = ({
     avatar, volunteerName, description, tags,
