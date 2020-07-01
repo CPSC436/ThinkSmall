c94b6b5da32f91da52e11b6cb73bee6c5782d048
Alice Kim <aliceykim0828@gmail.com>
2020-07-01 15:44:01 -0700

    align VolunteerCard styling

--- client/src/components/List/components/VolunteerCard.js
+++ client/src/components/List/components/VolunteerCard.js
@@ -5,36 +5,51 @@ import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-import Tags from '../../Tags/Tags'
-import classes from '../../../modules/card.module.css'
+import withStyles from '@material-ui/styles/withStyles'
+import Tags from './Tags/Tags'
+import classes from '../modules/card.module.css'
+
+const Text = withStyles({
+    root: {
+        fontFamily: '\'Baloo 2\', cursive',
+    },
+})(Typography)
 
 const VolunteerCard = ({
     avatar, volunteerName, description, tags,
 }) => (
-    <Card className={classes.root}>
-        <CardMedia
-            className={classes.media}
-            image={avatar}
-            title="Volunteer Picture"
-        />
-        <CardContent>
-            <Typography variant="h5" component="h6" style={{ fontFamily: '\'Baloo 2\', cursive' }}>
-                {volunteerName}
-            </Typography>
-            <Typography variant="subtitle1" component="h6">
+        <Card className={classes.root}>
+            <CardMedia
+                className={classes.media}
+                image={avatar}
+                title="Volunteer Picture"
+            />
+            <CardContent>
+                <Text variant="h5" component="h6">
+                    {volunteerName}
+                </Text>
                 <Tags tags={tags} />
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-                {description}
-            </Typography>
-            <Divider className="MuiDivider-root" light />
-        </CardContent>
-        <CardActions>
-            <Button size="small" color="primary" target="_blank">Read More</Button>
-            <Button size="small" color="primary" target="_blank">Start Conversation</Button>
-        </CardActions>
-    </Card>
-)
+                <Text gutterBottom variant="body2" color="textSecondary" component="p">
+                    {description}
+                </Text>
+            </CardContent>
+            <CardActions style={{ padding: 16 }}>
+                <Button
+                    className={classes.button}
+                    size="small" variant="contained" target="_blank"
+                    style={{ width: '100%', height: 36 }}
+                >
+                    Read More
+                </Button>
+                <Button
+                    className={classes.button}
+                    size="small" variant="contained" target="_blank"
+                    style={{ width: '100%', height: 36 }}
+                >
+                    Start Conversation
+                </Button>
+            </CardActions>
+        </Card>
+    )
 
 export default VolunteerCard
