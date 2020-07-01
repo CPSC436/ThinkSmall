770bec9b418a5501dba31ab4be72190a172f135e
Alice Kim <aliceykim0828@gmail.com>
2020-07-01 09:26:33 -0700

    save geolocation info when creating a new business

--- client/src/components/Form/BusinessForm.js
+++ client/src/components/Form/BusinessForm.js
@@ -33,13 +33,14 @@ const Form = ({ open = false, closeForm, addBusiness }) => {
     const [description, setDescription] = useState('')
     const [tags, setTags] = useState([])
     const [file, setFile] = useState()
+    const [geolocation, setGeolocation] = useState()
     const theme = useTheme()
     const fullScreen = useMediaQuery(theme.breakpoints.down('sm'))
 
     const onSelect = location => {
         geocodeByAddress(location)
             .then(results => getLatLng(results[0]))
-            .then(latLng => console.log('Success', latLng))
+            .then(latLng => setGeolocation(latLng))
             .then(() => setLocation(location))
             .catch(error => console.error('Error', error))
     }
@@ -58,6 +59,7 @@ const Form = ({ open = false, closeForm, addBusiness }) => {
             avatar,
             storeOwner: 'Dummy Name',
             location,
+            ...geolocation,
             description,
             needsHelp: true,
             tags,
