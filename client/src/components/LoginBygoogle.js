6d6e90be53adffb2df3cca934538ca66e1dc84ac
Hyesun An <hyesun@alumni.ubc.ca>
2020-07-05 17:43:27 -0700

    fixed by linter

--- client/src/components/LoginBygoogle.js
+++ client/src/components/LoginBygoogle.js
@@ -1,69 +1,72 @@
 import React, { useState, useEffect } from 'react'
-import GoogleLogin, { GoogleLogout } from 'react-google-login';
+import GoogleLogin, { GoogleLogout } from 'react-google-login'
 
 const LoginBygoogle = () => {
-  const [userDetails, setUserDetails] = useState({});
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+    const [userDetails, setUserDetails] = useState({})
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
 
-  const responseGoogle = response => {
-    setUserDetails(response.profileObj);
-    setIsUserLoggedIn(true);
-  }
+    const responseGoogle = response => {
+        setUserDetails(response.profileObj)
+        setIsUserLoggedIn(true)
+    }
 
-  const logout = () => {
-    setIsUserLoggedIn(false);
-  }
-  
-  useEffect(()=> {
-  }, [userDetails, isUserLoggedIn])
-  
-  return (
-    <div className="App">
-      {!isUserLoggedIn && (
-        <GoogleLogin
-          clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_KEY}
-          render={renderProps => (
-            <button
-              className="button"
-              onClick={renderProps.onClick}
-              disabled={renderProps.disabled}
-            >
-              Log in with Google
-            </button>
-          )}
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-        />
-      )}
-      {isUserLoggedIn && (
-        <div className="userDetails-wrapper">
-          <div className="details-wrapper">
-            <GoogleLogout
-              render={renderProps => (
-                <button
-                  className="logout-button"
-                  onClick={renderProps.onClick}
-                >
-                  Log Out
-                </button>
-              )}
-              onLogoutSuccess={logout}
-            />
-            <div className="image">
-              <img src={userDetails.imageUrl} />
-            </div>
-            <div className="name">
-              Welcome {userDetails.givenName}{" "}
-              {userDetails.familyName}
-            </div>
-            <div className="email"><i>{userDetails.email}</i></div>
-          </div>
-          <div className="bar" />
-          <div className="stand" />
+    const logout = () => {
+        setIsUserLoggedIn(false)
+    }
+
+    useEffect(() => {
+    }, [userDetails, isUserLoggedIn])
+
+    return (
+        <div className="App">
+            {!isUserLoggedIn && (
+                <GoogleLogin
+                    clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_KEY}
+                    render={renderProps => (
+                        <button
+                            className="button"
+                            onClick={renderProps.onClick}
+                            disabled={renderProps.disabled}
+                        >
+                            Log in with Google
+                        </button>
+                    )}
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                />
+            )}
+            {isUserLoggedIn && (
+                <div className="userDetails-wrapper">
+                    <div className="details-wrapper">
+                        <GoogleLogout
+                            render={renderProps => (
+                                <button
+                                    className="logout-button"
+                                    onClick={renderProps.onClick}
+                                >
+                                    Log Out
+                                </button>
+                            )}
+                            onLogoutSuccess={logout}
+                        />
+                        <div className="image">
+                            <img src={userDetails.imageUrl} />
+                        </div>
+                        <div className="name">
+                            Welcome
+                            {' '}
+                            {userDetails.givenName}
+                            {' '}
+                            {userDetails.familyName}
+                        </div>
+                        <div className="email"><i>{userDetails.email}</i></div>
+                    </div>
+                    <div className="bar" />
+                    <div className="stand" />
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );  
+    )
 }
 
 export default LoginBygoogle
