94ea37cf6475570cb7111b844675c42d01caa3ac
Alice Kim <aliceykim0828@gmail.com>
2020-07-09 14:30:50 -0700

    redirect unauthenticated user back to login page

--- api/routes/index.js
+++ api/routes/index.js
@@ -8,21 +8,7 @@ var user = require('./user')
 var isUserAuthenticated = require('../utils/isUserAuthenticated')
 
 router.get('/', (req, res) => res.render('index', { title: 'Express' }))
-router.get('/me', isUserAuthenticated, (req, res) => res.render('profile', {
-    user: req.user || {
-        "_id": "5f07789480f9ac18d50e7c8e",
-        "owns": [],
-        "tags": [],
-        "available": false,
-        "givenName": "Alice",
-        "familyName": "Kim",
-        "email": "alice828ubc@gmail.com",
-        "imageUrl": "https://lh3.googleusercontent.com/a-/AOh14Ggtu9ylzCEzHuUX4TWIWDA0_ta8HJGmabA8x3AX",
-        "createdAt": "2020-07-09T20:05:40.511Z",
-        "updatedAt": "2020-07-09T20:05:40.511Z",
-        "__v": 0
-    }
-}))
+router.get('/me', isUserAuthenticated, (req, res) => res.json(req.user))
 
 router.use('', auth)
 router.use('', business)
--- api/utils/isUserAuthenticated.js
+++ api/utils/isUserAuthenticated.js
@@ -1,19 +1,8 @@
-const log = require("debug")("oauth");
-
 module.exports = (req, res, next) => {
   // NOTE: if using jwt, you'd verify the token here.
-  // console.log(Object.keys(req))
-  // console.log(Object.keys(req.cookies))
-  // console.log(Object.keys(req.session))
-  // console.log(req._passport)
-  console.log('isUserAuthenticated', req.user)
-  // if (req.user) {
-  //   log(`authenticated ${req.user.profile.id}`);
-  //   next();
-  // } else {
-  //   log('user not authenticated');
-  //   // Path to start auth flow
-  //   res.redirect('/auth/google');
-  // }
-  next();
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/auth/google');
+  }
 };
