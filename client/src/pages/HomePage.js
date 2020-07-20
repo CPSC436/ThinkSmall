6da0449485045170e8b56f60d30337402d00b596
Alice Kim <aliceykim0828@gmail.com>
2020-07-19 19:33:14 -0700

    chore: add link to logo; center squares on HomePage

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -5,31 +5,33 @@ import classes from '../modules/home.module.css'
 const Square = ({
     title, subtitle, to, src,
 }) => (
-    <Link to={to}>
-        <div className={classes.square}>
-            <div>
-                <img src={src} className={classes.img} alt={title} />
-                <h3>{title}</h3>
-                <p style={{ fontSize: 'small' }}>{subtitle}</p>
+        <Link to={to}>
+            <div className={classes.square}>
+                <div>
+                    <img src={src} className={classes.img} alt={title} />
+                    <h3>{title}</h3>
+                    <p style={{ fontSize: 'small' }}>{subtitle}</p>
+                </div>
             </div>
-        </div>
-    </Link>
-)
+        </Link>
+    )
 
 const Home = () => (
-    <div className={classes.root}>
-        <Square
-            title="Business Owner"
-            subtitle="We need a nice logo for our website!"
-            to="/business"
-            src="https://images.unsplash.com/photo-1464869372688-a93d806be852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-        />
-        <Square
-            title="Volunteer"
-            subtitle="Looking to get involved in the community!"
-            to="/volunteer"
-            src="https://images.unsplash.com/photo-1560252829-804f1aedf1be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-        />
+    <div className={classes.root} >
+        <div className={classes.container} >
+            <Square
+                title="Business Owner"
+                subtitle="We need a nice logo for our website!"
+                to="/business"
+                src="https://images.unsplash.com/photo-1464869372688-a93d806be852?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            />
+            <Square
+                title="Volunteer"
+                subtitle="Looking to get involved in the community!"
+                to="/volunteer"
+                src="https://images.unsplash.com/photo-1560252829-804f1aedf1be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
+            />
+        </div>
     </div>
 )
 
