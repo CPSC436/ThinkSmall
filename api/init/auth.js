f0ab6f096591913570ded39281fdd48b199032e2
John <johnskimca@gmail.com>
2020-08-04 17:12:45 -0700

    If we refresh the page after login, then login works!

--- api/init/auth.js
+++ api/init/auth.js
@@ -67,7 +67,6 @@ module.exports = (app) => {
                 newUser.password = newUser.generateHash(req.body.password)
                 newUser.givenName = req.body.givenName
                 newUser.familyName = req.body.familyName
-                newUser.available = true
 
                 // save the user
                 newUser.save(function(err) {
@@ -94,29 +93,25 @@ module.exports = (app) => {
 
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
+
         User.findOne({ email :  req.body.email }, function(err, user) {
           // if there are any errors, return the error before anything else
           if (err)
             return done(err)
-
           // if no user is found, return the message
+
           if (!user)
             return done(null, false, req.flash('loginMessage', 'No user found.')) // req.flash is the way to set flashdata using connect-flash
 
           // if the user is found but the password is wrong
-          if (!user.validPassword(req.body.password))
+          if (!user.validPassword(req.body.password)){
             return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')) // create the loginMessage and save it to session as flashdata
-
+          }
           // all is well, return successful user
           return done(null, user)
         })
 
       }))
-
-
-
-
-
 
   // OAuth Strategy to get access_token
   passport.use(
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const passport = require('passport')
 const User = require('../models/user')
+const bodyParser = require('body-parser')
 
 
 const router = express.Router()
+router.use(bodyParser.urlencoded({ extended: true }))
 const scope = ['email', 'profile']
 
 // Oauth Routes
@@ -24,11 +26,19 @@ router.post('/register', passport.authenticate('local-signup', {
     })
 )
 
-router.post('/login', passport.authenticate('local-login', {
+router.post('/login' ,passport.authenticate('local-login', {
     successRedirect : '/',
     failureRedirect : '/about',
     failureFlash : true
-}));
+}))
 
+
+// router.post('/login', function(req, res, next) {
+//     passport.authenticate('local-login', function(err, user, info) {
+//         console.log('succecss')
+//         console.log(user)
+//         console.log(req.user)
+//     })(req, res, next);
+// });
 
 module.exports = router
